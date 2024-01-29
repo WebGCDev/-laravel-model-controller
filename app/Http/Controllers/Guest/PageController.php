239d5dba9b462eec537950c31cf55b204efa23d3
WebGCDev <gaetano.chi@gmail.com>
2024-01-29 20:13:01 +0100

    Add : Loading First Query

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -10,6 +11,11 @@ class PageController extends Controller
     //
     public function index()
     {
+
+        $Movies = Movie::all();
+
+        dd($Movies);
+
         return view('welcome');
     }
 }
